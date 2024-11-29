99ac01710d63a0a258581e145bcda88d0cebac7a
sebastian-raubach <sebastian.raubach@hutton.ac.uk>
2024-11-29 14:29:45 +0000

    - FIX: Sub-path issues router.

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,7 +11,7 @@ import { setupLayouts } from 'virtual:generated-layouts'
 import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(location.pathname),
   routes: setupLayouts(routes),
 })
 
